576b09385fb0bb8c1085a434d69689e53edcfade
Suhdo <dungvipnr@gmail.com>
2023-03-30 14:42:39 +0700

    feat: install react-navigation-header-buttons

--- App_Client/screens/mainScreens/Home.jsx
+++ App_Client/screens/mainScreens/Home.jsx
@@ -3,16 +3,39 @@ import {
     Text,
     View,
     FlatList,
-    SafeAreaView
+    SafeAreaView,
+    Button,
+    TouchableOpacity
 } from 'react-native'
 import React, { useState } from 'react'
 import ListItem from '../../components/ListItem'
 import Data from '../../data/data.json'
+import Icon from 'react-native-vector-icons/Ionicons'
 
 const Home = ({ navigation, route }) => {
+    const [count, setCount] = useState(0);
+
+    React.useEffect(() => {
+        navigation.setOptions({
+            headerRight: () => (
+                <>
+                    <TouchableOpacity onPress={() => setCount((c) => c + 1)} style={{ marginEnd: 8 }}>
+                        <Icon name='ios-search-sharp' size={24} color={'#666666'} />
+                    </TouchableOpacity>
+
+                    <TouchableOpacity onPress={() => setCount((c) => c + 1)} >
+                        <Icon name='ellipsis-vertical' size={24} color={'#666666'} />
+                    </TouchableOpacity>
+                </>
+            ),
+            headerTintColor: '#666666',
+        });
+    }, [navigation])
+
 
     return (
         <SafeAreaView style={styles.container}>
+            <Text>Count: {count}</Text>
             <FlatList
                 numColumns={2}
                 data={Data.items}
